0fc58ab28afc84dca16329cac7a8ed8a96962980
carlos.murillo.pagador@alumnos.upm.es <carlos.murillo.pagador@alumnos.upm.es>
2021-12-09 12:13:54 +0100

    Constrains prueba+act. modo crono

--- project_1/project_1.srcs/sources_1/new/Modo_Crono.vhd
+++ project_1/project_1.srcs/sources_1/new/Modo_Crono.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 entity Modo_Crono is
     Port (
@@ -18,10 +19,10 @@ entity Modo_Crono is
 end Modo_Crono;
 
 architecture Behavioral of Modo_Crono is
-    signal unit_sec : std_logic_vector(3 downto 0);
-    signal dec_sec : std_logic_vector(3 downto 0);
-    signal unit_min : std_logic_vector(3 downto 0);
-    signal dec_min : std_logic_vector(3 downto 0);
+    --signal unit_sec : std_logic_vector(3 downto 0);
+    --signal dec_sec : std_logic_vector(3 downto 0);
+    --signal unit_min : std_logic_vector(3 downto 0);
+    --signal dec_min : std_logic_vector(3 downto 0);
     
     signal clk_1hz : std_logic;
     
@@ -39,11 +40,32 @@ begin
     );
     
     process (clk_1hz)
+    subtype V is integer range 0 to 15;
+    variable unit_sec : V :=0;
+    variable unit_min : V :=0;
+    variable dec_sec : V :=0;
+    variable dec_min : V :=0;
     begin
         if rising_edge(clk_1hz) then
+            unit_sec:=unit_sec+1;
+            if unit_sec=10 then
+            unit_sec:=0;
+            end if;
+            
         end if;
+        code8 <= std_logic_vector(to_unsigned(unit_sec,code8'length));
+        code7 <= std_logic_vector(to_unsigned(dec_sec,code7'length));
+        code6 <= std_logic_vector(to_unsigned(unit_min,code6'length));
+        code5 <= std_logic_vector(to_unsigned(dec_sec,code5'length));
     end process;
     
+    process 
+    begin
+    code1<="1001";
+    code2<="1001";
+    code3<="1111";
+    code4<="1111";
+    end process;
     
     
 end Behavioral;
